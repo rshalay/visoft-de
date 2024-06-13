df6a1e7f82b4c1ca745637db1406fe9c3111ae9a
Roman <r.shalay@gmail.com>
2024-06-13 18:59:12 +0300

    45

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,15 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  
+  reporter: [
+    ['list'],
+    ['@estruyf/github-actions-reporter', {
+      title: 'My custom title',
+      useDetails: true,
+      showError: true
+    }]
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
